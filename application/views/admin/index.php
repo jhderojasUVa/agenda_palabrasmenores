1b6a55adaf054b4a715e1903f29b0e109ad44e90
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2016-11-07 14:36:41 -0500

    Update index.php

---
+++ application/views/admin/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<?
+if (isset($error)) {
+  ?><span style="color:red;">LA HAS CAGAO, USUARIO o COTNRASÑEA MAL</span><?
+}
+?>
+<form action="/admin/index/index" method="POST">
+<input type="text" name="usuario"><br/>
+<input type="password" name="password"><br/>
+<input type="submit" value="enviar">
+</form>
+</body>
+</html>
